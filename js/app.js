7077a5c0b12df29f2f6a25fe54bf4fee9bfe23d0
Bella Dear <arabelladear@hotmail.co.uk>
2018-08-25 17:17:43 +0100

    fixed scroll for all windows

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
 $(() => {
 
-  const $home = $('.home-link');
-  const $about = $('.about-link');
-  const $work = $('.work-link');
-  const $contact = $('.contact-link');
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-  const scroll = function(position) {
-    window.scroll({
-      top: position,
-      left: 0,
-      behavior: 'smooth'
-    });
-  };
-
-  $home.click(() => scroll(0));
-  $about.click(() => scroll(600));
-  $work.click(() => scroll(950));
-  $contact.click(() => scroll(2300));
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top-100
+    }, 700);
+  });
 
 });
