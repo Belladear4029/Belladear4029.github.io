3ac41e267fc3093d21c8df303a0cd6f899881dca
Arabella Dear <arabella.dear@sapient.com>
2020-09-20 10:28:29 +0100

    made pans rotate on scroll

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ $(() => {
     );
   });
 
+  window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
+
   window.addEventListener('resize', function (event) {
     setCarouselWidth();
     setSelectedSlideIndex(selectedSlideIndex);
